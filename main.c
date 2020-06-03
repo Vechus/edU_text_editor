1f9450a075e8876c07e88959ea0935ea694aa124
Luca Vecchio <luca1.vecchio@mail.polimi.it>
2020-06-03 18:07:31 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,215 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+#define INPUT_MAX_LENGTH 1024
+#define INIT_LINES_SIZE 2
+
+enum cmd_type {CHANGE, DELETE, PRINT, UNDO, REDO, QUIT};
+
+typedef struct {
+    enum cmd_type type;
+    unsigned long int args[2];
+}cmd;
+
+typedef struct txt_line_s {
+    char* content;
+    struct txt_line_s* next;
+}txt_line_t;
+
+/*typedef struct line_head_s {
+    txt_line_t* line;
+}line_head_t;*/
+
+typedef struct lines_stack_s{
+    unsigned long int size;
+    txt_line_t** lines;
+}lines_stack_t;
+
+typedef struct cmd_stack_node_s{
+    cmd* command;
+    struct cmd_stack_node_s* next;
+}cmd_stack_node_t;
+
+
+void debug_print_cmd(cmd* c) {
+    enum cmd_type t = c->type;
+    if(t==CHANGE||t==DELETE||t==PRINT) {
+        printf("CMD::%d: %lu, %lu\n", t, c->args[0], c->args[1]);
+    } else if(t!=QUIT) {
+        printf("CMD::%d: %lu\n", t, c->args[0]);
+    } else {
+        printf("QUIT\n");
+    }
+}
+
+void debug_print_lines_stack(lines_stack_t* linesStack) {
+    for(int i = 0; i < linesStack->size; i++) {
+        if(linesStack->lines[i] == NULL) continue;
+        if(linesStack->lines[i]->next != NULL) {
+            txt_line_t* tmp = linesStack->lines[i];
+            printf("%s -> ", tmp->content);
+            tmp = tmp->next;
+            while(tmp != NULL) {
+                printf("%s; ", tmp->content);
+                tmp = tmp->next;
+            }
+            printf("\n");
+        } else {
+            printf("%s\n", linesStack->lines[i]->content);
+        }
+    }
+}
+
+void debug_print_cmd_stack(cmd_stack_node_t* cmdStack) {
+    cmd_stack_node_t* tmp = cmdStack;
+    printf("Commands: \n");
+    while(tmp != NULL) {
+        debug_print_cmd(tmp->command);
+        tmp = tmp->next;
+    }
+}
+
+void push_cmd(cmd_stack_node_t** cmdStack, cmd* command) {
+    cmd_stack_node_t* tmp = (cmd_stack_node_t *)malloc(sizeof(cmd_stack_node_t));
+    tmp->command = command;
+    tmp->next = *cmdStack;
+    *cmdStack = tmp;
+}
+
+void push_line(txt_line_t** stack, char* text) {
+    txt_line_t* tmp;
+    tmp = (txt_line_t *)malloc(sizeof(txt_line_t));
+    tmp->content = (char *)malloc(strlen(text) * sizeof(char));
+    strcpy(tmp->content, text);
+    tmp->next = *stack;
+    *stack = tmp;
+}
+
+void handle_change(lines_stack_t* linesStack, cmd *command) {
+    char* input;
+    char buff[INPUT_MAX_LENGTH];
+
+    if(command->args[1] > linesStack->size) {
+        linesStack->lines = (txt_line_t **)realloc(linesStack->lines, (command->args[1] + 1) * sizeof(txt_line_t*));
+        linesStack->size = command->args[1] + 1;
+    }
+
+    for(unsigned long int i = command->args[0] - 1; i <= command->args[1] - 1; i++) {
+        if(linesStack->lines[i] == NULL)
+            linesStack->lines[i] = (txt_line_t *)malloc(sizeof(txt_line_t));
+        fgets(buff, INPUT_MAX_LENGTH, stdin);
+        input = (char *)malloc(strlen(buff) * sizeof(char));
+        strcpy(input, buff);
+        push_line(&(linesStack->lines[i]), input);
+    }
+    // .\n
+    getchar_unlocked();
+    getchar_unlocked();
+}
+
+cmd* parse_cmd() {
+    char c;
+    int arg1 = 0, arg2 = 0;
+    cmd *ret = (cmd*) malloc(sizeof(cmd));
+    bool a1 = false;
+    c = getchar_unlocked();
+    int i = 0;
+    while(c != '\n') {
+        if(a1 == false && isdigit(c)) {
+            arg1 += (int) (c - 48) * (int) pow(10, i);
+            i++;
+            c = getchar_unlocked();
+            continue;
+        } else if(isdigit(c)) {
+            arg2 += (int) (c - 48) * (int) pow(10, i);
+            i++;
+            c = getchar_unlocked();
+            continue;
+        }
+        switch (c) {
+            case ',':
+                a1 = true;
+                i = 0;
+                break;
+            case 'q':
+                ret->type = QUIT;
+                break;
+            case 'u':
+                ret->type = UNDO;
+               //ret->args = (unsigned long int*) malloc(sizeof(int));
+                ret->args[0] = arg1;
+                break;
+            case 'r':
+                ret->type = REDO;
+                //ret->args = (unsigned long int*) malloc(sizeof(int));
+                ret->args[0] = arg1;
+                break;
+            case 'c':
+                ret->type = CHANGE;
+                //ret->args = (unsigned long int*) malloc(2 * sizeof(int));
+                ret->args[0] = arg1;
+                ret->args[1] = arg2;
+                break;
+            case 'p':
+                ret->type = PRINT;
+                //ret->args = (unsigned long int*) malloc(2 * sizeof(int));
+                ret->args[0] = arg1;
+                ret->args[1] = arg2;
+                break;
+            case 'd':
+                ret->type = DELETE;
+                //ret->args = (unsigned long int*) malloc(2 * sizeof(int));
+                ret->args[0] = arg1;
+                ret->args[1] = arg2;
+                break;
+            default:
+                puts("\nInvalid command format.\n");
+                break;
+        }
+        c = getchar_unlocked();
+    }
+    return ret;
+}
+
+int main() {
+    cmd* command;
+    char* dot_ptr = ".";
+    char* ptr;
+    // all lines with history
+    lines_stack_t* linesStack;
+    // command history
+    cmd_stack_node_t* cmdStack;
+
+    cmdStack = (cmd_stack_node_t *)malloc(sizeof(cmd_stack_node_t));
+    cmdStack->next = NULL;
+
+    linesStack = (lines_stack_t *)malloc(sizeof(lines_stack_t));
+    linesStack->lines = (txt_line_t **)malloc(INIT_LINES_SIZE * sizeof(txt_line_t*));
+    linesStack->size = INIT_LINES_SIZE;
+    /*for(int i = 0; i < INIT_LINES_SIZE; i ++) {
+        linesStack->lines[i] = (txt_line_t *)malloc(sizeof(txt_line_t));
+    }*/
+
+    printf("DEBUG\n");
+
+    command = parse_cmd();
+    cmdStack->command = command;
+    while(command->type != QUIT) {
+        switch (command->type) {
+            case CHANGE:
+                handle_change(linesStack, command);
+                break;
+            default:
+                break;
+        }
+        push_cmd(&cmdStack, command);
+        command = parse_cmd();
+    }
+    debug_print_cmd_stack(cmdStack);
+    debug_print_lines_stack(linesStack);
+    return 0;
+}
